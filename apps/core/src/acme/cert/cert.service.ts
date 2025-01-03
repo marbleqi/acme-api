16cc8c25bed24072a6c9c1efa5fbee406f049a20
marbleqi <marbleqi@163.com>
2025-01-03 09:57:13 +0800

    完成acme包中相关方法调用的验证

--- apps/core/src/acme/cert/cert.service.ts
+++ apps/core/src/acme/cert/cert.service.ts
@@ -36,39 +36,84 @@ export class CertService extends CommonService<
 
   /**启动后初始化 */
   async onApplicationBootstrap(): Promise<void> {
-    await this.sync();
+    console.debug('启动后初始化');
+    // await this.sync();
 
     /**账户信息 */
-    const accountDB = await this.accountSrv.show(1);
+    const account = await this.accountSrv.show(1);
+    console.debug('账户信息', account);
     /**DNS服务商 */
     const provider = await this.keySrv.show(1);
-    /**创建 ACME 客户端 */
+    console.debug('云服务商信息', provider);
+    // /**创建 ACME 客户端 */
     const client = new Client({
-      directoryUrl: accountDB.config.staging
+      directoryUrl: account.config.staging
         ? directory.letsencrypt.staging
         : directory.letsencrypt.production,
-      accountKey: accountDB.accountKey,
+      accountKey: account.accountKey,
     });
-    const account = await client.createAccount({
+    console.debug('云服务商信息', client);
+    const result = await client.createAccount({
       termsOfServiceAgreed: true,
-      contact: [`mailto:${accountDB.config.email}`],
+      contact: [`mailto:${account.config.email}`],
     });
-    console.log('账户注册成功', account);
+    console.debug('ACME账号注册成功', result);
 
     const order = await client.createOrder({
-      identifiers: [{ type: 'dns', value: '*.marbleqi.top' }],
+      identifiers: [{ type: 'dns', value: '*.api.marbleqi.top' }],
     });
-    console.debug('order', order);
+    console.debug('订单创建成功', order);
 
-    const auths = await client.getAuthorizations(order);
-    console.debug('auths', auths);
-    for (const auth of auths) {
-      const challenges = auth.challenges;
-      console.debug('challenges', challenges);
-      for (const challenge of challenges) {
+    const authorizations = await client.getAuthorizations(order);
+    console.debug('授权信息', authorizations);
+
+    for (const authz of authorizations) {
+      const challenge = authz.challenges.find((c) => c.type === 'dns-01');
+      if (challenge) {
         const dnsRecordValue =
           await client.getChallengeKeyAuthorization(challenge);
-        console.debug('dnsRecordValue', challenge, dnsRecordValue);
+        // const dnsRecordName = `_acme-challenge.${authz.identifier.value.replace(/\.?marbleqi\.top$/, '')}`;
+        const dnsRecordName = `_acme-challenge.api`;
+        console.debug('DNS记录', dnsRecordName, dnsRecordValue);
+        // 添加 DNS 记录
+        await this.aliSrv.create(
+          1,
+          'marbleqi.top',
+          dnsRecordName,
+          'TXT',
+          dnsRecordValue,
+        );
+        console.debug('已添加DNS记录', dnsRecordName, dnsRecordValue);
+        // 标记挑战为已完成
+        await client.completeChallenge(challenge);
+      }
+    }
+
+    for (const authz of authorizations) {
+      await client.waitForValidStatus(authz);
+      console.debug('授权验证成功', authz);
+    }
+
+    const [key, csr] = await crypto.createCsr({
+      commonName: '*.api.marbleqi.top',
+    });
+
+    await client.finalizeOrder(order, csr);
+    console.debug('证书申请成功', order);
+
+    const certificate = await client.getCertificate(order);
+    console.debug('获取证书', certificate);
+    console.debug('证书私钥', key.toString());
+
+    for (const authz of authorizations) {
+      const challenge = authz.challenges.find((c) => c.type === 'dns-01');
+      if (challenge) {
+        // const dnsRecordName = `_acme-challenge.${authz.identifier.value.replace(/\.?marbleqi\.top$/, '')}`;
+        const dnsRecordName = `_acme-challenge.api`;
+
+        // 删除 DNS 记录
+        await this.aliSrv.delete(1, 'marbleqi.top', dnsRecordName);
+        console.debug('删除 DNS 记录', dnsRecordName, '成功');
       }
     }
   }
